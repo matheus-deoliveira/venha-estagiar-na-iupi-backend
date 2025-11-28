d472c60ccac5921d27183f3b6f0db9af8d5db11d
Matheus de Oliveira <matheusdeoliveiraes@gmail.com>
2025-11-28 14:07:47 -0300

    fix urls

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from core.views import TransactionViewSet
+from core.views.summary import TransactionSummaryView
 
 # O Router cria as rotas automaticamente,
 # fazendo um gerênciamento desses endpoints:
@@ -15,4 +16,5 @@ router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('summary/', TransactionSummaryView.as_view(), name='summary'),
 ]
--- core/views/summary.py
+++ core/views/summary.py
@@ -5,6 +5,7 @@ from core.models import Transaction
 
 class TransactionSummaryView(APIView):
     def get(self, request):
+        # A parte de "or 0" é para caso não tenha nada, ao invés de dar erro ele mostra 0 como resultado
         income = Transaction.objects.filter(type='income').aggregate(Sum('amount'))['amount__sum'] or 0
         expense = Transaction.objects.filter(type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
         
